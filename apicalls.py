62bd6eda7813aec67d4bb5678fa80e7293b773e7
anhtuanluu <atuanbk58@gmail.com>
2023-06-16 15:39:04 +0700

    update v2

--- apicalls.py
+++ apicalls.py
@@ -1,20 +1,40 @@
 import requests
+import os
+import sys
+import logging
+import requests
+import json
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
+with open('config.json','r') as f:
+    config = json.load(f) 
+
+test_data_path = os.path.join(config['test_data_path']) 
+model_path = os.path.join(config['output_model_path']) 
 #Specify a URL that resolves to your workspace
-URL = "http://127.0.0.1/"
+URL = "http://127.0.0.1:8000"
 
+logging.info(f"Post request /prediction")
+response1 = requests.post(
+    f'{URL}/prediction',
+    json={'filepath': os.path.join(test_data_path, 'testdata.csv')}).text
 
+logging.info("Get scoring")
+response2 = requests.get(f'{URL}/scoring').text
 
-#Call each API endpoint and store the responses
-response1 = #put an API call here
-response2 = #put an API call here
-response3 = #put an API call here
-response4 = #put an API call here
+logging.info("Get summarystats")
+response3 = requests.get(f'{URL}/summarystats').text
 
-#combine all API responses
-responses = #combine reponses here
+logging.info("Get diagnostics")
+response4 = requests.get(f'{URL}/diagnostics').text
 
-#write the responses to your workspace
-
-
-
+logging.info("Save report")
+with open(os.path.join(model_path, 'apireturns.txt'), 'w') as file:
+    file.write('Predictions:\n')
+    file.write(response1)
+    file.write('Score:\n')
+    file.write(response2)
+    file.write('\nSummarystats:\n')
+    file.write(response3)
+    file.write('Diagnostics:\n')
+    file.write(response4)
--- reporting.py
+++ reporting.py
@@ -1,32 +1,46 @@
-import pickle
-from sklearn.model_selection import train_test_split
+"""
+Generate a confusion matrix
+"""
 import pandas as pd
-import numpy as np
-from sklearn import metrics
 import matplotlib.pyplot as plt
 import seaborn as sns
 import json
 import os
-
-
+import sys
+import logging
+import diagnostics
+from sklearn.metrics import confusion_matrix
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 ###############Load config.json and get path variables
 with open('config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-
-
-
+test_data_path = os.path.join(config['test_data_path']) 
+model_path = os.path.join(config['output_model_path']) 
 
 ##############Function for reporting
-def score_model():
-    #calculate a confusion matrix using the test data and the deployed model
-    #write the confusion matrix to the workspace
+def plot_confusion_matrix():
+    """
+    calculate a confusion matrix using the test data and the deployed model
+    write the confusion matrix to the workspace
+    """
+    logging.info("Load data")
+    df = pd.read_csv(os.path.join(test_data_path, 'testdata.csv'))
+    y_test = df.pop('exited')
+    X_test = df.drop(['corporation'], axis=1)
 
+    logging.info("Test data")
+    y_pred = diagnostics.model_predictions(X_test)
 
-
-
+    logging.info("Save confusion matrix")
+    confusion_matrices = confusion_matrix(y_test, y_pred)
+    classes = ["0", "1"]
+    df_cfm = pd.DataFrame(confusion_matrices, index = classes, columns = classes)
+    plt.figure(figsize = (10,7))
+    plt.title('Confusion Matrix')
+    cfm_plot = sns.heatmap(df_cfm, annot=True)
+    cfm_plot.figure.savefig(os.path.join(model_path, "confusionmatrix.png"))
 
 if __name__ == '__main__':
-    score_model()
+    plot_confusion_matrix()
